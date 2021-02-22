b45ddaafead23017e4a94431ce80119f87d4309e
Daniel Otaviano <danielpontes0@icloud.com>
2021-02-22 08:56:11 -0300

    feat: add some routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
-import express from 'express'
+import express, { Router } from 'express'
 
 const app = express()
+
+const router = Router()
+app.use(router)
+
+router.get('/', (req, res) => {
+  return res.json({ hello: 'word' })
+})
+
+router.post('/', (req, res) => {
+  return res.json({ message: 'Os dados foram salvos com sucesso!' })
+})
 
 app.listen(3000, () =>
   console.log('Server is Running!'))
